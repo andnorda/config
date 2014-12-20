eade27c1596fb8e94724ec764f5da6e763fa97fa
Andreas Nordahl <andnorda@gmail.com>
2014-12-20 13:24:59 +0100

    added integration pom profile

---
+++ src/test/java/config/ApplicationIT.java
@@ -0,0 +1,13 @@
+package config;
+
+import org.junit.Test;
+
+import static org.junit.Assert.fail;
+
+public class ApplicationIT {
+
+    @Test
+    public void test() throws Exception {
+        fail();
+    }
+}
