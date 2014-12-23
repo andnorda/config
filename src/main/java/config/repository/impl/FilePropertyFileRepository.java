ed289f74d299a8db3222b7bce8d41b0be6660ca3
Andreas Nordahl <andnorda@gmail.com>
2014-12-23 18:55:13 +0100

    merged FilePropertyFileRepoITs

--- src/main/java/config/repository/impl/FilePropertyFileRepository.java
+++ src/main/java/config/repository/impl/FilePropertyFileRepository.java
@@ -13,24 +13,21 @@ import java.util.List;
 import java.util.Map;
 
 public class FilePropertyFileRepository extends FileRepository implements PropertyFileRepository {
-    @Override
-    public void update(PropertyFileDto propertyFileDto, String... path) {
-        List<String> dirs = Arrays.asList(path);
-        String fileName = dirs.get(dirs.size() - 1);
-        dirs = dirs.subList(0, dirs.size() - 1);
-        File file = baseDir;
-        for (String dir : dirs) {
-            file = getSubDirWithName(file, dir);
-        }
-        getPropertyFileWithName(file, fileName);
-        writeNewPropertiesToFile(propertyFileDto, file, fileName);
-    }
-
     private final File baseDir;
 
     public FilePropertyFileRepository(File baseDir) {
         super(baseDir);
         this.baseDir = baseDir;
+    }
+
+    @Override
+    public Collection<PropertyFileDto> getAll(String... path) {
+        List<String> dirs = Arrays.asList(path);
+        File file = baseDir;
+        for (String dir : dirs) {
+            file = getSubDirWithName(file, dir);
+        }
+        return getAllPropertyFiles(file);
     }
 
     @Override
@@ -46,13 +43,16 @@ public class FilePropertyFileRepository extends FileRepository implements Proper
     }
 
     @Override
-    public Collection<PropertyFileDto> getAll(String... path) {
+    public void update(PropertyFileDto propertyFileDto, String... path) {
         List<String> dirs = Arrays.asList(path);
+        String fileName = dirs.get(dirs.size() - 1);
+        dirs = dirs.subList(0, dirs.size() - 1);
         File file = baseDir;
         for (String dir : dirs) {
             file = getSubDirWithName(file, dir);
         }
-        return getAllPropertyFiles(file);
+        getPropertyFileWithName(file, fileName);
+        writeNewPropertiesToFile(propertyFileDto, file, fileName);
     }
 
     private void writeNewPropertiesToFile(PropertyFileDto propertyFileDto, File parent, String fileName) {
--- src/test/java/config/repository/FilePropertyFileRepositoryIT.java
+++ src/test/java/config/repository/FilePropertyFileRepositoryIT.java
@@ -14,75 +14,17 @@ import org.junit.runner.RunWith;
 
 import java.io.File;
 import java.io.PrintWriter;
+import java.util.HashMap;
 
+import static org.hamcrest.CoreMatchers.hasItems;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.empty;
 import static org.hamcrest.core.Is.is;
 
 @RunWith(Enclosed.class)
-public class FileVersionPropertyFileRepositoryIT {
+public class FilePropertyFileRepositoryIT {
 
     public static class GetOne {
-        private FilePropertyFileRepository repo;
-        private File baseDir;
-
-        @Before
-        public void setUp() throws Exception {
-            baseDir = new File("repo");
-            baseDir.mkdir();
-            repo = new FilePropertyFileRepository(baseDir);
-        }
-
-        @After
-        public void tearDown() throws Exception {
-            FileUtils.deleteDirectory(baseDir);
-        }
-
-        @Test (expected = NotFound.class)
-        public void throwsNotFound_GivenNoApp() throws Exception {
-            repo.get("app", "version", "file");
-        }
-
-        @Test (expected = NotFound.class)
-        public void throwsNotFound_GivenNoVersion() throws Exception {
-            // Given
-            new File("repo/app").mkdir();
-
-            // When
-            repo.get("app", "version", "file");
-        }
-
-        @Test (expected = NotFound.class)
-        public void throwsNotFound_GivenNoFile() throws Exception {
-            // Given
-            new File("repo/app/version").mkdirs();
-
-            // When
-            repo.get("app", "version", "file");
-        }
-
-        @Test (expected = NotFound.class)
-        public void throwsNotFound_GivenDirFile() throws Exception {
-            // Given
-            new File("repo/app/version/file").mkdirs();
-
-            // When
-            repo.get("app", "version", "file");
-        }
-
-        @Test
-        public void returnsPropertyFile() throws Exception {
-            // Given
-            new File("repo/app/version").mkdirs();
-            new File("repo/app/version/file").createNewFile();
-            new File("repo/app/version/file").mkdir();
-
-            // Then
-            assertThat(repo.get("app", "version", "file").getName(), is("file"));
-        }
-    }
-
-    public static class GetAll {
         private PropertyFileRepository repo;
         private File baseDir;
 
@@ -100,7 +42,7 @@ public class FileVersionPropertyFileRepositoryIT {
 
         @Test (expected = NotFound.class)
         public void throwsNotFound_GivenNoApp() throws Exception {
-            repo.getAll("app", "version");
+            repo.get("app", "file");
         }
 
         @Test (expected = NotFound.class)
@@ -108,33 +50,54 @@ public class FileVersionPropertyFileRepositoryIT {
             // Given
             new File("repo/app").mkdir();
 
-            // Then
-            repo.getAll("app", "version");
+            // When
+            repo.get("app", "version", "file");
         }
 
-        @Test
-        public void returnsEmptyCollection() throws Exception {
+        @Test (expected = NotFound.class)
+        public void throwsNotFound_GivenNoInstance() throws Exception {
             // Given
             new File("repo/app/version").mkdirs();
 
-            // Then
-            assertThat(repo.getAll("app", "version"), is(empty()));
+            // When
+            repo.get("app", "version", "instance", "file");
+        }
+
+        @Test (expected = NotFound.class)
+        public void throwsNotFound_GivenNoFile() throws Exception {
+            // Given
+            new File("repo/app/version/instance").mkdirs();
+
+            // When
+            repo.get("app", "version", "instance", "file");
+        }
+
+        @Test (expected = NotFound.class)
+        public void throwsNotFound_GivenDirFile() throws Exception {
+            // Given
+            new File("repo/app/version/instance/file").mkdirs();
+
+            // When
+            repo.get("app", "version", "instance", "file");
         }
 
         @Test
-        public void returnsCollectionWithOneFile() throws Exception {
+        public void returnsPropertyFile() throws Exception {
             // Given
-            new File("repo/app/version").mkdirs();
+            new File("repo/app/version/instance").mkdirs();
+            new File("repo/app/file").createNewFile();
             new File("repo/app/version/file").createNewFile();
-            new File("repo/app/version/notAFile").mkdir();
+            new File("repo/app/version/instance/file").createNewFile();
 
             // Then
-            assertThat(repo.getAll("app", "version").size(), is(1));
-            assertThat(repo.getAll("app", "version").iterator().next().getName(), is("file"));
+            PropertyFileDto file = new PropertyFileDto("file", ImmutableMap.of());
+            assertThat(repo.get("app", "file"), is(file));
+            assertThat(repo.get("app", "version", "file"), is(file));
+            assertThat(repo.get("app", "version", "instance", "file"), is(file));
         }
     }
 
-    public static class Update {
+    public static class GetAll {
         private FilePropertyFileRepository repo;
         private File baseDir;
 
@@ -152,7 +115,74 @@ public class FileVersionPropertyFileRepositoryIT {
 
         @Test (expected = NotFound.class)
         public void throwsNotFound_GivenNoApp() throws Exception {
-            repo.update(new PropertyFileDto("file", ImmutableMap.of("key", "new")), "app", "version", "file");
+            repo.getAll("app");
+        }
+
+        @Test (expected = NotFound.class)
+        public void throwsNotFound_GivenNoVersion() throws Exception {
+            // Given
+            new File("repo/app").mkdir();
+
+            // Then
+            repo.getAll("app", "version");
+        }
+
+        @Test (expected = NotFound.class)
+        public void throwsNotFound_GivenNoInstance() throws Exception {
+            // Given
+            new File("repo/app/version").mkdirs();
+
+            // Then
+            repo.getAll("app", "version", "instance");
+        }
+
+        @Test
+        public void returnsEmptyCollection() throws Exception {
+            // Given
+            new File("repo/app/version/instance").mkdirs();
+
+            // Then
+            assertThat(repo.getAll("app"), is(empty()));
+            assertThat(repo.getAll("app", "version"), is(empty()));
+            assertThat(repo.getAll("app", "version", "instance"), is(empty()));
+        }
+
+        @Test
+        public void returnsCollectionWithOneFile() throws Exception {
+            // Given
+            new File("repo/app/version/instance").mkdirs();
+            new File("repo/app/file").createNewFile();
+            new File("repo/app/version/file").createNewFile();
+            new File("repo/app/version/instance/file").createNewFile();
+
+            // Then
+            PropertyFileDto file = new PropertyFileDto("file", ImmutableMap.of());
+            assertThat(repo.getAll("app"), hasItems(file));
+            assertThat(repo.getAll("app", "version"), hasItems(file));
+            assertThat(repo.getAll("app", "version", "instance"), hasItems(file));
+        }
+    }
+
+    public static class Update {
+        private FilePropertyFileRepository repo;
+        private File baseDir;
+        private PropertyFileDto file = new PropertyFileDto("file", ImmutableMap.of("key", "new"));
+
+        @Before
+        public void setUp() throws Exception {
+            baseDir = new File("repo");
+            baseDir.mkdir();
+            repo = new FilePropertyFileRepository(baseDir);
+        }
+
+        @After
+        public void tearDown() throws Exception {
+            FileUtils.deleteDirectory(baseDir);
+        }
+
+        @Test (expected = NotFound.class)
+        public void throwsNotFound_GivenNoApp() throws Exception {
+            repo.update(file, "app", "file");
         }
 
         @Test (expected = NotFound.class)
@@ -161,31 +191,40 @@ public class FileVersionPropertyFileRepositoryIT {
             new File("repo/app").mkdir();
 
             // When
-            repo.update(new PropertyFileDto("file", ImmutableMap.of("key", "new")), "app", "version", "file");
+            repo.update(file, "app", "version", "file");
+        }
+
+        @Test (expected = NotFound.class)
+        public void throwsNotFound_GivenNoInstance() throws Exception {
+            // Given
+            new File("repo/app/version").mkdirs();
+
+            // When
+            repo.update(file, "app", "version", "instance", "file");
         }
 
         @Test (expected = NotFound.class)
         public void throwsNotFound_GivenNoFile() throws Exception {
             // Given
-            new File("repo/app/version").mkdirs();
+            new File("repo/app/version/instance").mkdirs();
 
             // When
-            repo.update(new PropertyFileDto("file", ImmutableMap.of("key", "new")), "app", "version", "file");
+            repo.update(file, "app", "version", "instance", "file");
         }
 
         @Test
         public void updatesProperty() throws Exception {
             // Given
-            new File("repo/app/version").mkdirs();
-            PrintWriter writer = new PrintWriter("repo/app/version/file", "UTF-8");
+            new File("repo/app/version/instance").mkdirs();
+            PrintWriter writer = new PrintWriter("repo/app/version/instance/file", "UTF-8");
             writer.println("key=old");
             writer.close();
 
             // When
-            repo.update(new PropertyFileDto("file", ImmutableMap.of("key", "new")), "app", "version", "file");
+            repo.update(file, "app", "version", "instance", "file");
 
             // Then
-            assertThat(repo.get("app", "version", "file"), CoreMatchers.is(new PropertyFileDto("file", ImmutableMap.of("key", "new"))));
+            assertThat(repo.get("app", "version", "instance", "file"), is(file));
         }
     }
 }
